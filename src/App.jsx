1de12c275e3a77fa01548f84edc3f9ab12491e92
Flow2510 <sendra.florian@gmail.com>
2025-08-13 15:14:53 +0200

    initialisation du projet

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Counter from './components/counter/counter'
+import Header from './components/header/header'
+
+function App() {
+
+
+  return (
+    <>
+      <Header />
+      <main>
+        <Counter />
+      </main>
+    </>
+  )
+}
+
+export default App
