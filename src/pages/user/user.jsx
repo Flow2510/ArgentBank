177f9841a82627dbd2823bfaa6987b1cf66bca74
Flow2510 <sendra.florian@gmail.com>
2025-08-20 18:40:56 +0200

    ajout du mode edit username

--- src/pages/user/user.jsx
+++ src/pages/user/user.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { setInfo } from '../../data/store';
 import Account from '../../components/account/account';
@@ -8,6 +8,11 @@ export default function User() {
   const dispatch = useDispatch();
   const token = useSelector(state => state.user.token);
   const info = useSelector(state => state.user.info);
+  const [editMode, setEditMode] = useState(false);
+
+  useEffect(() => {
+    console.log(info)
+  })
 
   useEffect(() => {
     if (!token) return; // on attend d’avoir le token
@@ -28,14 +33,47 @@ export default function User() {
       .catch(err => console.error(err));
   }, [token, dispatch]);
 
+  const handleClick = (e) => {
+    e.preventDefault();
+    setEditMode(false);
+  }
+
   return (
     <main>
       <div className='user__wrapper'>
         {info && (
           <section className='user'>
             <div className='user__content'>
-              <h1 className='user__content-title'>Welcome back<br />{info.firstName} {info.lastName} !</h1>
-              <button className='user__content-button'>Edit Name</button>
+              {!editMode && 
+                <>
+                  <h1 className='user__content-title'>Welcome back<br />{info.firstName} {info.lastName} !</h1>
+                  <button className='user__content-button' onClick={()=> setEditMode(true)}>Edit Name</button>
+                </>
+              }
+              {editMode &&
+                <>
+                  <h1 className='user__content-title'>Edit user info</h1>
+                  <form className='user__form' action="">
+                    <div className='user__input-wrapper'>
+                      <label htmlFor="userNameInput">User name:</label>
+                      <input type="text" name='userNameInput' id='userNameInput' placeholder={info.userName}/>
+                    </div>
+                    <div className='user__input-wrapper'>
+                      <label htmlFor="firstNameInput">First name:</label>
+                      <input type="text" name='firstNameInput' id='firstNameInput' placeholder={info.firstName} disabled/>
+                    </div>
+                    <div className='user__input-wrapper'>
+                      <label htmlFor="lastNameInput">Last name:</label>
+                      <input type="text" name='lastNameInput' id='lastNameInput' placeholder={info.lastName} disabled/>
+                    </div>
+                    <div className='user__button-wrapper'>
+                      <button>Save</button>
+                      <button onClick={handleClick}>Cancel</button>
+                    </div>
+                  </form>
+
+                </>
+              }
             </div>
             <div className='user__gallery'>
               <Account 
